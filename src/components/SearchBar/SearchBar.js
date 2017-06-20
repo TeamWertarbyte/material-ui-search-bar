285bd37b3534b39262d43d4e640ac353a8ce7e90
Remi Bernal <bernal.rs@gmail.com>
2017-06-20 16:21:38 +0100

    Wired up prettier.

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,52 +1,52 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import { AutoComplete, IconButton, Paper } from 'material-ui'
-import SearchIcon from 'material-ui/svg-icons/action/search'
-import CloseIcon from 'material-ui/svg-icons/navigation/close'
-import { grey500 } from 'material-ui/styles/colors'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { AutoComplete, IconButton, Paper } from 'material-ui';
+import SearchIcon from 'material-ui/svg-icons/action/search';
+import CloseIcon from 'material-ui/svg-icons/navigation/close';
+import { grey500 } from 'material-ui/styles/colors';
 
 const getStyles = (props, state) => {
-  const { disabled }  = props;
+  const { disabled } = props;
   const { value } = state;
-  const nonEmpty = value ? 0 : value.length
+  const nonEmpty = value ? 0 : value.length;
 
   return {
     root: {
       height: 48,
       display: 'flex',
-      justifyContent: 'space-between'
+      justifyContent: 'space-between',
     },
     iconButtonClose: {
       style: {
-        opacity: ! disabled ? 0.54 : 0.38,
+        opacity: !disabled ? 0.54 : 0.38,
         transform: nonEmpty ? 'scale(1, 1)' : 'scale(0, 0)',
-        transition: 'transform 200ms cubic-bezier(0.4, 0.0, 0.2, 1)'
+        transition: 'transform 200ms cubic-bezier(0.4, 0.0, 0.2, 1)',
       },
       iconStyle: {
         opacity: nonEmpty ? 1 : 0,
-        transition: 'opacity 200ms cubic-bezier(0.4, 0.0, 0.2, 1)'
+        transition: 'opacity 200ms cubic-bezier(0.4, 0.0, 0.2, 1)',
       },
     },
     iconButtonSearch: {
       style: {
-        opacity: ! disabled ? 0.54 : 0.38,
+        opacity: !disabled ? 0.54 : 0.38,
         transform: nonEmpty ? 'scale(0, 0)' : 'scale(1, 1)',
         transition: 'transform 200ms cubic-bezier(0.4, 0.0, 0.2, 1)',
-        marginRight: -48
+        marginRight: -48,
       },
       iconStyle: {
         opacity: nonEmpty ? 0 : 1,
-        transition: 'opacity 200ms cubic-bezier(0.4, 0.0, 0.2, 1)'
+        transition: 'opacity 200ms cubic-bezier(0.4, 0.0, 0.2, 1)',
       },
     },
     input: {
-      width: '100%'
+      width: '100%',
     },
     searchContainer: {
       margin: 'auto 16px',
-      width: '100%'
-    }
-  }
+      width: '100%',
+    },
+  };
 };
 
 /**
@@ -57,12 +57,13 @@ export default class SearchBar extends Component {
   static defaultProps = {
     closeIcon: <CloseIcon color={grey500} />,
     dataSource: [],
-    dataSourceConfig: {text: 'text', value: 'value'},
+    dataSourceConfig: { text: 'text', value: 'value' },
     hintText: 'Search',
     searchIcon: <SearchIcon color={grey500} />,
     disabled: false,
     value: '',
-  }
+    style: null,
+  };
 
   static propTypes = {
     /** Override the close icon. */
@@ -84,55 +85,55 @@ export default class SearchBar extends Component {
     /** The value of the text field. */
     value: PropTypes.string,
     /** Disables text field. */
-    disabled: PropTypes.bool
-  }
+    disabled: PropTypes.bool,
+  };
 
   static contextTypes = {
     muiTheme: PropTypes.object.isRequired,
-  }
+  };
 
   state = {
     focus: false,
     value: this.props.value,
     active: false,
-  }
+  };
 
-  componentWillReceiveProps(nextProps){
+  componentWillReceiveProps(nextProps) {
     if (this.props.value !== nextProps.value) {
       this.setState({ ...this.state, value: nextProps.value });
     }
   }
 
   handleFocus = () => {
-    this.setState({focus: true})
-  }
+    this.setState({ focus: true });
+  };
 
   handleBlur = () => {
-    this.setState({focus: false})
+    this.setState({ focus: false });
     if (this.state.value.trim().length === 0) {
-      this.setState({value: ''})
+      this.setState({ value: '' });
     }
-  }
+  };
 
-  handleInput = (e) => {
-    this.setState({value: e})
-    this.props.onChange(e)
-  }
+  handleInput = e => {
+    this.setState({ value: e });
+    this.props.onChange(e);
+  };
 
   handleCancel = () => {
-    this.setState({active: false, value: ''})
-    this.props.onChange('')
-  }
+    this.setState({ active: false, value: '' });
+    this.props.onChange('');
+  };
 
-  render () {
+  render() {
     const styles = getStyles(this.props, this.state);
-    const value = this.state.value
+    const value = this.state.value;
 
     return (
       <Paper
         style={{
           ...styles.root,
-          ...this.props.style
+          ...this.props.style,
         }}
       >
         <div style={styles.searchContainer}>
@@ -147,7 +148,8 @@ export default class SearchBar extends Component {
             underlineShow={false}
             dataSource={this.props.dataSource}
             dataSourceConfig={this.props.dataSourceConfig}
-            disabled={this.props.disabled}/>
+            disabled={this.props.disabled}
+          />
         </div>
         <IconButton
           onTouchTap={this.props.onRequestSearch}
@@ -166,6 +168,6 @@ export default class SearchBar extends Component {
           {this.props.closeIcon}
         </IconButton>
       </Paper>
-    )
+    );
   }
 }
