3cc54147a0b136aabe386864a4b37de122ee1d97
Victor Login <batazor111@gmail.com>
2018-05-17 18:42:56 +0300

    Upgrade to material-ui@1.0.0-rc.1 (#44)

--- src/styleguide/Wrapper.js
+++ src/styleguide/Wrapper.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import { MuiThemeProvider } from '@material-ui/core/styles'
 
 export default class Wrapper extends Component {
   render () {
