424bc993c4a15b6cbf7327223e1f1a9f7538d38f
Getulio Valentin Sánchez <valentin2507@gmail.com>
2018-07-29 18:06:19 -0400

    Improve TypeScript definitions (#52)
    * Include className as a prop
    
    * Include all properties
    
    * Improve TypeScript definitions
    
    * Update author notice in the typings and use semicolons consistently.

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for Material Search Bar
 // Project: https://github.com/TeamWertarbyte/material-ui-search-bar
-// Definitions by: [Tyler Kellogg] <recurrence@gmail.com>
+// Original definitions by: [Tyler Kellogg] <recurrence@gmail.com>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 /// <reference types="react" />
@@ -8,17 +8,38 @@
 declare module 'material-ui-search-bar' {
   export interface SearchBarProps {
       /**
+       * Whether to clear search on escape.
+       */
+      cancelOnEscape?: boolean;
+      /**
+       * Override or extend the styles applied to the component.
+       */
+      classes?: {
+        root?: string,
+        iconButton?: string,
+        iconButtonHidden?: string,
+        iconButtonDisabled?: string,
+        searchIconButton?: string,
+        icon?: string,
+        input?: string,
+        searchContainer?: string
+      };
+      /**
+       * Custom top-level class.
+       */
+      className?: string;
+      /**
        * Override the close icon.
        */
-      closeIcon?: any;
+      closeIcon?: JSX.Element;
       /**
        * Disables text field.
        */
-      disabled?: any;
+      disabled?: boolean;
       /**
-       * Sets placeholder for the embedded text field.
+       * Fired when the search is cancelled.
        */
-      placeholder?: any;
+      onCancelSearch?(): void;
       /**
        * Fired when the text value changes.
        */
@@ -28,13 +49,17 @@ declare module 'material-ui-search-bar' {
        */
       onRequestSearch?(): void;
       /**
+       * Sets placeholder for the embedded text field.
+       */
+      placeholder?: string;
+      /**
        * Override the search icon.
        */
-      searchIcon?: any;
+      searchIcon?: JSX.Element;
       /**
        * Override the inline-styles of the root element.
        */
-      style?: any;
+      style?: object;
       /**
        * The value of the text field.
        */
